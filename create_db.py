5883e5c314d310b53afee187f560da34e8edb039
akirakyle <akyle@cmu.edu>
2016-05-16 00:00:14 -0400

    started pandas db creation, never finished

--- create_db.py
+++ create_db.py
@@ -2,6 +2,9 @@ import pandas as pd
 
 ngrams_prefix = '/home/akyle/ngram_data/eng_1gram_'
 
+#ToDo: get data into a Pandas Data frame so that
+# perhaps the contains method can be used. See
+# (http://pandas.pydata.org/pandas-docs/stable/text.html)
 def get_freq(word, date):
     ngram_file = ngrams_prefix + word[0].lower()
     f = open(ngram_file, 'r')
